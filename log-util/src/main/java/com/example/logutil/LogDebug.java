15fc6d0355d63cc63ccb87f50e8aff85a26503ee
arnold5sweet <arnold.kmulera@gmail.com>
2017-10-04 11:02:16 +0300

    For creating Logs

--- log-util/src/main/java/com/example/logutil/LogDebug.java
+++ log-util/src/main/java/com/example/logutil/LogDebug.java
@@ -1,8 +1,16 @@
 package com.example.logutil;
 
+import android.util.Log;
+
 /**
  * Created by Arnold on 10/3/2017.
  */
 
 public class LogDebug {
+    private static final String TAG = "SUPER AWESOME APP";
+
+    public static void d (String message){
+        Log.d(TAG, message);
+
+    }
 }
